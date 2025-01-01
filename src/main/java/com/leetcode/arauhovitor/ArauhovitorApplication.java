fb2eeb9aeda4adce2289f6ad3a821684caab9bcd
Vitor Araujo <kvitorrsantos@gmail.com>
2025-01-01 17:19:26 -0300

    [Top Interview 150 - Array/String] Remove Element

---
+++ src/main/java/com/leetcode/arauhovitor/ArauhovitorApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.arauhovitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArauhovitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArauhovitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leetcode/arauhovitor/ArauhovitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.arauhovitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArauhovitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
