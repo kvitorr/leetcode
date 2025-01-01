5c7033b47af54839d030f4abd561aa84ee9fa6ce
Vitor Araujo <kvitorrsantos@gmail.com>
2025-01-01 17:21:24 -0300

    fix: change name folder array_spring -> array_string

--- src/main/java/com/leetcode/arauhovitor/array_string/removeElement.java
+++ src/main/java/com/leetcode/arauhovitor/array_string/removeElement.java
@@ -1,4 +1,4 @@
-package com.leetcode.arauhovitor.array_spring;
+package com.leetcode.arauhovitor.array_string;
 /*
 * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
